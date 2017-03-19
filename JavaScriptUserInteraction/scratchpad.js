49db7a9051399bd9a848be6ba27b3504a10be447
Dylan Cox <dylancox37@gmail.com>
2017-03-19 18:40:10 +0000

    Adding the User Interaction files

---
+++ JavaScriptUserInteraction/scratchpad.js
@@ -0,0 +1,161 @@
+function fibhelper(n){
+    var value;
+    var div = document.createElement('div');
+    div.setAttribute("class", "fib");
+    if(n < 2){
+        if(n === 0)
+        {
+            value = 0;
+        }
+        else if(n === 1)
+        {
+            value = 1;
+        }
+        var p = document.createElement('p');
+        p.textContent = 'Fib(' +n+ ') = ' +value;
+        div.appendChild(p);
+    }
+    
+    else
+    {
+        var left = fibhelper(n - 1);
+        var temp = left.html.getAttribute("class");
+        left.html.setAttribute("class", temp +" fib-left");
+        
+        var right = fibhelper(n - 2);
+        temp = right.html.getAttribute("class");
+        right.html.setAttribute("class", temp +" fib-right");
+        
+        value = left.value + right.value;
+        var p = document.createElement('p');
+        p.textContent = 'Fib(' +n+ ') = ' +value;
+        div.appendChild(p);
+        
+        div.appendChild(left.html);
+        div.appendChild(right.html);
+    }
+    return {'value' : value, 'html' : div};
+}
+
+var fib = function(num, node)
+{
+    var tree = fibhelper(num);
+    node.appendChild(tree.html);
+    node.setAttribute('id', 'fib');
+}
+
+function pellHelper(n)
+{
+    var value;
+    var div = document.createElement('div');
+    div.setAttribute('class', 'pell');
+    if(n < 2){
+        if(n === 0)
+        {
+            value = 0;
+        }
+        else if(n === 1)
+        {
+            value = 1;
+        }
+        var p = document.createElement('p');
+        p.textContent = "Pell(" + n + ") = " + value;
+        div.appendChild(p);
+    }
+    else
+    {
+        var left = pellHelper(n-1);
+        var temp = left.html.getAttribute('class');
+        left.html.setAttribute('class', temp + ' pell-left');
+        
+        var right = pellHelper(n-2);
+        temp = right.html.getAttribute('class');
+        right.html.setAttribute('class', temp+ ' pell-right');
+        
+        value = (2 * left.value) + right.value;
+        var p = document.createElement('p');
+        p.textContent = 'Pell(' +n+ ') = ' +value;
+        div.appendChild(p);
+        
+        div.appendChild(left.html);
+        div.appendChild(right.html);
+    }
+    return {'value' : value, 'html' : div};
+}
+
+var pell = function(num, node)
+{
+    var tree = pellHelper(num);
+    node.appendChild(tree.html);
+    node.setAttribute('id', 'pell');
+}
+
+
+function tribHelper(n)
+{
+    var value;
+    var div = document.createElement('div');
+    div.setAttribute("class", "trib");
+    if(n <= 2)
+    {
+        if(n === 0 || n ===1)
+        {
+            value = 0;
+        }
+        else if(n ===2)
+        {
+            value = 1;
+        }
+        var p = document.createElement('p');
+        p.textContent = 'Trib(' +n+ ') = ' +value;
+        div.appendChild(p);
+    }
+    else
+    {
+        var left = tribHelper(n-1);
+        var temp = left.html.getAttribute('class');
+        left.html.setAttribute('class', temp + ' trib-left');
+        
+        var center = tribHelper(n-2);
+        temp = center.html.getAttribute('class');
+        center.html.setAttribute('class', temp+ ' trib-center');
+        
+        var right = tribHelper(n-3);
+        temp = right.html.getAttribute('class');
+        right.html.setAttribute('class', temp+ ' trib-right');
+        
+        value = left.value + center.value + right.value;
+        var p = document.createElement('p');
+        p.textContent = 'Trib(' +n+ ') = ' +value;
+        div.appendChild(p);
+        
+        div.appendChild(left.html);
+        div.appendChild(center.html);
+        div.appendChild(right.html);
+    }
+    return {'value' : value, 'html' : div};
+}
+
+var trib = function(num, node)
+{
+    var tree = tribHelper(num);
+    node.appendChild(tree.html);
+    node.setAttribute('id', 'trib');
+}
+
+
+var fibdiv = document.createElement('div');
+fibdiv.setAttribute("class", "fib-box");
+document.body.appendChild(fibdiv);
+
+var pelldiv = document.createElement('div');
+pelldiv.setAttribute("class", "pell-box");
+document.body.appendChild(pelldiv);
+
+var tribdiv = document.createElement('div');
+tribdiv.setAttribute("class", "trib-box");
+document.body.appendChild(tribdiv);
+
+fib(6, document.querySelector('.fib-box'));
+pell(6, document.querySelector('.pell-box'));
+trib(6, document.querySelector('.trib-box'));

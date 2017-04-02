1d727e095196e93f950b599c187f22b6eadb47b5
Dylan Cox <dylancox37@gmail.com>
2017-04-02 01:29:19 +0000

    Adding django project

---
+++ hello_django/hello/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from time import gmtime, strftime
+# Create your views here.
+
+def index(response):
+    time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    return HttpResponse('Hello World! Time is: ' +time)
